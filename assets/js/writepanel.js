f13015b31229da5f67c055f79f559fbabab4a918
Justin Shreve <justin.shreve@gmail.com>
2015-11-02 08:43:41 -0800

    Correctly show/hide the accommodations sections when applicable

--- assets/js/writepanel.js
+++ assets/js/writepanel.js
@@ -1,11 +1,16 @@
 jQuery( function( $ ) {
 
+	$( '.hide_if_accommodation_booking' ).hide();
+	$( '.show_if_accommodation_booking' ).hide();
+
 	$( 'select#product-type' ).change( function () {
 		var select_val = $( this ).val();
 
 		if ( 'accommodation-booking' === select_val ) {
+			$( '.show_if_accommodation_booking' ).show();
 			$( '.hide_if_accommodation_booking' ).hide();
 		} else {
+			$( '.show_if_accommodation_booking' ).hide();
 			$( '.hide_if_accommodation_booking' ).show();
 		}
 	} ).change();
