d8b96eb618c476520f492e73d6dd587e9adae187
Justin Shreve <justin.shreve@gmail.com>
2016-01-12 07:19:36 -0800

    Fix spacing for file include, fix quote usage when calculating times. Also prevent saving of check-in/check-out times for non accommodation products.

--- includes/admin/class-wc-accommodation-booking-admin-meta-box.php
+++ includes/admin/class-wc-accommodation-booking-admin-meta-box.php
@@ -3,21 +3,33 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * Overwrites pieces of the booking details (edit booking/manage booking) page
+ */
 class WC_Accommodation_Booking_Admin_Meta_Box {
-
 	public function __construct() {
 		add_filter( 'woocommerce_booking_admin_start_time', array( $this, 'start_time' ), 10, 2 );
 		add_filter( 'woocommerce_booking_admin_end_time', array( $this, 'end_time' ), 10, 2 );
 	}
 
-	public function start_time( $time, $product_id ) {
+	public function start_time( $time, $post_id ) {
+		$product_id = get_post_meta( $post_id, '_booking_product_id', true );
+		$product = wc_get_product( get_post_meta( $post_id, '_booking_product_id', true ) );
+		if ( 'accommodation-booking' !== $product->product_type ) {
+			return $time;
+		}
 		$check_in = get_option( 'woocommerce_accommodation_bookings_check_in', '' );
-		return date_i18n( get_option( 'time_format' ), strtotime( "Today " . $check_in ) );
+		return date_i18n( get_option( 'time_format' ), strtotime( 'Today ' . $check_in ) );
 	}
 
-	public function end_time( $time, $product_id ) {
+	public function end_time( $time, $post_id ) {
+		$product_id = get_post_meta( $post_id, '_booking_product_id', true );
+		$product = wc_get_product( get_post_meta( $post_id, '_booking_product_id', true ) );
+		if ( 'accommodation-booking' !== $product->product_type ) {
+			return $time;
+		}
 		$check_out = get_option( 'woocommerce_accommodation_bookings_check_out', '' );
-		return date_i18n( get_option( 'time_format' ), strtotime( "Today " . $check_out ) );
+		return date_i18n( get_option( 'time_format' ), strtotime( 'Today ' . $check_out ) );
 	}
 }
 
