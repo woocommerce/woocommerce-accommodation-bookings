71fdbfe122a9252605a9f57ade391ca25b73385e
Robert Staddon <robert@abundantdesigns.com>
2016-03-03 12:05:32 -0600

    Allow this product type to have resources
    Stop has_resources() function from returning false

--- includes/class-wc-product-accommodation-booking.php
+++ includes/class-wc-product-accommodation-booking.php
@@ -62,14 +62,6 @@ class WC_Product_Accommodation_Booking extends WC_Product_Booking {
 	}
 
 	/**
-	 * Version 1.0.0 does not extra resources
-	 * @return boolean
-	 */
-	public function has_resources() {
-		return false;
-	}
-
-	/**
 	 * We want users to be able to select their range of dates
 	 * @return boolean
 	 */
