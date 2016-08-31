e66f3fec5d809816d28485e49546e35f9f9b8485
Akeda Bagus <admin@gedex.web.id>
2016-08-31 09:41:23 +0700

    Update changelog and bump version for #69.

--- woocommerce-accommodation-bookings.php
+++ woocommerce-accommodation-bookings.php
@@ -3,7 +3,7 @@
 Plugin Name: WooCommerce Accommodation Bookings
 Plugin URI: https://woocommerce.com/products/woocommerce-accommodation-bookings/
 Description: An accommodations add-on for the WooCommerce Bookings extension.
-Version: 1.0.5
+Version: 1.0.6
 Author: WooThemes
 Author URI: https://woocommerce.com
 Text Domain: woocommerce-accommodation-bookings
@@ -19,5 +19,5 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 require_once( 'includes/class-wc-accommodation-bookings-plugin.php' );
-$plugin = new WC_Accommodation_Bookings_Plugin( __FILE__, '1.0.5' );
+$plugin = new WC_Accommodation_Bookings_Plugin( __FILE__, '1.0.6' );
 $plugin->run();
