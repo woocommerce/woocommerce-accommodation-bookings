f67bfafc0ec90d7ccfe031fd588a7b361fa2f26f
roykho <roykho77@gmail.com>
2019-07-01 15:51:32 -0700

    Check for bookings core version for bw compat

--- includes/class-wc-accommodation-booking-date-picker.php
+++ includes/class-wc-accommodation-booking-date-picker.php
@@ -138,7 +138,12 @@ class WC_Accommodation_Booking_Date_Picker {
 
 		// Using all existing bookings we will calculate start and end time for each booking.
 		// Those times will be considered for switching particular day from full to partially booked days.
-		$existing_bookings  = WC_Booking_Data_Store::get_all_existing_bookings( $product );
+		if ( version_compare( WC_BOOKINGS_VERSION, '1.15.0', '<' ) ) {
+			$existing_bookings  = WC_Bookings_Controller::get_all_existing_bookings( $product );
+		} else {
+			$existing_bookings  = WC_Booking_Data_Store::get_all_existing_bookings( $product );
+		}
+
 		foreach ( $existing_bookings as $booking ) {
 
 			$resource   = $booking->get_resource_id();
--- includes/class-wc-product-accommodation-booking.php
+++ includes/class-wc-product-accommodation-booking.php
@@ -221,7 +221,11 @@ class WC_Product_Accommodation_Booking extends WC_Product_Booking {
 
 			list( $interval, $base_interval ) = $intervals;
 
-			$existing_bookings = WC_Booking_Data_Store::get_all_existing_bookings( $bookable_product, $from, $to );
+			if ( version_compare( WC_BOOKINGS_VERSION, '1.15.0', '<' ) ) {
+				$existing_bookings = WC_Bookings_Controller::get_all_existing_bookings( $bookable_product, $from, $to );
+			} else {
+				$existing_bookings = WC_Booking_Data_Store::get_all_existing_bookings( $bookable_product, $from, $to );
+			}
 
 			$booking_resource = $resource_id ? $bookable_product->get_resource( $resource_id ) : null;
 			$available_slots  = array();
