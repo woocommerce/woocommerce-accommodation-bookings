efcd2ec2b9c192267d134c2324a9ac9f45dffffa
mikkamp <michael.kamp@automattic.com>
2020-02-04 17:24:29 +0000

    Bumping version

--- woocommerce-accommodation-bookings.php
+++ woocommerce-accommodation-bookings.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce Accommodation Bookings
  * Plugin URI: https://woocommerce.com/products/woocommerce-accommodation-bookings/
  * Description: An accommodations add-on for the WooCommerce Bookings extension.
- * Version: 1.1.12
+ * Version: 1.1.13
  * Author: WooCommerce
  * Author URI: https://woocommerce.com
  * Text Domain: woocommerce-accommodation-bookings
@@ -11,7 +11,7 @@
  * WC tested up to: 3.8
  * WC requires at least: 2.6
  *
- * Copyright: © 2018 WooCommerce.
+ * Copyright: © 2020 WooCommerce
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
@@ -20,7 +20,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'WC_ACCOMMODATION_BOOKINGS_VERSION', '1.1.12' ); // WRCS: DEFINED_VERSION.
+define( 'WC_ACCOMMODATION_BOOKINGS_VERSION', '1.1.13' ); // WRCS: DEFINED_VERSION.
 
 require_once( 'includes/class-wc-accommodation-bookings-plugin.php' );
 $plugin = new WC_Accommodation_Bookings_Plugin( __FILE__, WC_ACCOMMODATION_BOOKINGS_VERSION );
