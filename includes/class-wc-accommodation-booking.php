9ecca92c4b0a134a78bbb2ae3470a1141b2cfc0f
Boro Sitnikovski <buritomath@gmail.com>
2020-09-18 14:10:15 +0200

    Add filter to skip formatting

--- includes/class-wc-accommodation-booking.php
+++ includes/class-wc-accommodation-booking.php
@@ -25,6 +25,31 @@ class WC_Accommodation_Booking {
 		add_filter( 'woocommerce_data_stores', array( $this, 'register_data_stores' ), 10 );
 		add_filter( 'woocommerce_bookings_apply_multiple_rules_per_block', array( $this, 'disable_overlapping_rates' ), 10, 2 );
 		add_filter( 'woocommerce_bookings_resource_duration_display_string', array( $this, 'filter_resource_duration_display_string' ), 10, 2 );
+		add_filter( 'woocommerce_bookings_ics_format_date', array( $this, 'disable_ics_formatting_for_accommodation' ), 10, 2 );
+	}
+
+	/**
+	 * Skip formatting the date for accommodation bookings products.
+	 *
+	 * @param int        $value     Formatted value.
+	 * @param int        $timestamp Timestamp to format.
+	 * @param WC_Booking $booking   Booking object.
+	 *
+	 * @return string Formatted date for ICS.
+	 */
+	public function disable_ics_formatting_for_accommodation( $value, $timestamp, $booking ) {
+		if ( ! $booking ) {
+			return $value;
+		}
+
+		$product = $booking->get_product();
+
+		if ( ! is_a( $product, 'WC_Product_Accommodation_Booking' ) ) {
+			// Return original, unformatted value
+			return $timestamp;
+		}
+
+		return $value;
 	}
 
 	/**
