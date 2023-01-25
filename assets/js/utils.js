9908ba43cac08e46dace100b2d1af9e99fc13dc3
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2023-01-25 16:37:35 +0530

    fix: use correct variable name

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -21,5 +21,5 @@ export function is_product_type_accommodation_booking( $booking_form ) {
  * @return {object}
  */
 export function get_booking_form( $field ) {
-	return $form.closest( 'form.wc-bookings-booking-form' );
+	return $field.closest( 'form.wc-bookings-booking-form' );
 }
