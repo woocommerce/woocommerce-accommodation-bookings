3ec1c54c583ffb8acd40a8d5405230848e965e61
faisal-alvi <faisalalvi33@gmail.com>
2023-09-12 19:47:39 +0530

    update php to 7.4

--- woocommerce-accommodation-bookings.php
+++ woocommerce-accommodation-bookings.php
@@ -12,7 +12,7 @@
  * Requires at least: 6.1
  * WC tested up to: 8.0
  * WC requires at least: 7.8
- * Requires PHP: 7.3
+ * Requires PHP: 7.4
  *
  * Copyright: © 2023 WooCommerce
  * License: GNU General Public License v3.0
