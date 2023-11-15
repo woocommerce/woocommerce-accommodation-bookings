5af291fd373d903fefff180319369058d0da55d6
Sumit Bagthariya <67687255+qasumitbagthariya@users.noreply.github.com>
2023-11-15 15:15:08 +0530

    Update woocommerce-accommodation-bookings.php
    wc bump version

--- woocommerce-accommodation-bookings.php
+++ woocommerce-accommodation-bookings.php
@@ -9,9 +9,9 @@
  * Text Domain: woocommerce-accommodation-bookings
  * Domain Path: /languages
  * Tested up to: 6.3
- * Requires at least: 6.1
- * WC tested up to: 8.0
- * WC requires at least: 7.8
+ * Requires at least: 6.2
+ * WC tested up to: 8.3
+ * WC requires at least: 8.1
  * Requires PHP: 7.3
  *
  * Copyright: © 2023 WooCommerce
