d65f9b9c3b3427c745275cd94e98cfe9f04c1255
Peter Wilson <git-commit-by@peterwilson.cc>
2025-10-17 09:59:52 +1100

    Indicate tested up to PHP 8.4.

--- woocommerce-accommodation-bookings.php
+++ woocommerce-accommodation-bookings.php
@@ -13,7 +13,7 @@
  * Requires at least: 6.7
  * WC tested up to: 10.2
  * WC requires at least: 10.0
- * PHP tested up to: 8.3
+ * PHP tested up to: 8.4
  * Requires PHP: 7.4
  *
  * Copyright: © 2023 WooCommerce
