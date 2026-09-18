de7ef34f38b69653cb7e3b7b439bcd3a2a758f1a
Ján Mikláš <neosinner@gmail.com>
2026-09-18 15:26:58 +0200

    style: format accommodation JavaScript

--- src/js/writepanel.js
+++ src/js/writepanel.js
@@ -1,7 +1,7 @@
 import jQuery from 'jquery';
 
-jQuery(function ($) {
-	$('#rates_rows').sortable({
+jQuery( function ( $ ) {
+	$( '#rates_rows' ).sortable( {
 		items: 'tr',
 		cursor: 'move',
 		axis: 'y',
@@ -11,28 +11,30 @@ jQuery(function ($) {
 		helper: 'clone',
 		opacity: 0.65,
 		placeholder: 'wc-metabox-sortable-placeholder',
-		start(event, ui) {
-			ui.item.css('background-color', '#f6f6f6');
+		start( event, ui ) {
+			ui.item.css( 'background-color', '#f6f6f6' );
 		},
-		stop(event, ui) {
-			ui.item.removeAttr('style');
+		stop( event, ui ) {
+			ui.item.removeAttr( 'style' );
 		},
-	});
+	} );
 
 	function wc_accommodation_bookings_trigger_change_events() {
-		$('#_wc_accommodation_booking_has_restricted_days').trigger('change');
+		$( '#_wc_accommodation_booking_has_restricted_days' ).trigger(
+			'change'
+		);
 	}
 
-	$('#_wc_accommodation_booking_has_restricted_days').on(
+	$( '#_wc_accommodation_booking_has_restricted_days' ).on(
 		'change',
 		function () {
-			if ($(this).is(':checked')) {
-				$('.booking-day-restriction').show();
+			if ( $( this ).is( ':checked' ) ) {
+				$( '.booking-day-restriction' ).show();
 			} else {
-				$('.booking-day-restriction').hide();
+				$( '.booking-day-restriction' ).hide();
 			}
 		}
 	);
 
 	wc_accommodation_bookings_trigger_change_events();
-});
+} );
